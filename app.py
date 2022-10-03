731f762829b10ff9bba9e504f55d74026d36762a
dhonihanif <dhonihanif354@gmail.com>
2022-10-03 16:49:09 +0700

    new

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from http.client import REQUEST_HEADER_FIELDS_TOO_LARGE
+from unittest import result
 import numpy as np
 import pandas as pd
 from tensorflow.keras.preprocessing.text import Tokenizer
@@ -6,66 +6,73 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 import tensorflow as tf
 import re
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, flash, redirect
 import cv2
 from keras.models import load_model
-from PIL import Image
-<<<<<<< HEAD
-import base64
-import io
-=======
-from tensorflow.keras.preprocessing import image
->>>>>>> c2dbe869 (new)
+from PIL import Image, ImageOps
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from werkzeug.utils import secure_filename
 
+import io
+import os
 import cv2
 
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-model = load_model("./expression_detection/facial_emotions_model.h5")
-model.compile(loss="binary_crossentropy",
-optimizer="rmsprop", metrics=["accuracy"])
-classes=['Angry','Disgust','Fear','Happy','Neutral','Sad','Surprise']
+app = Flask(__name__)                                    #calling
+def get_model():
+    global model
+    model = load_model('./expression_detection/facial_emotions_model.h5')
+    print("Model loaded!")
 
-app = Flask(__name__, template_folder="templates")
-def allowed_file(filename):
-	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+def load_image(img_path):
 
-@app.route("/", methods=["GET", "POST"])
-def hello_world():
-    return render_template("index.html")
+    img = load_img(img_path, target_size=(48, 48), color_mode='grayscale')
+    img_tensor = img_to_array(img)                    # (height, width, channels)
+    img_tensor= img_tensor.reshape(1,48,48,1)
+        
+    return img_tensor
 
-@app.route("/post/predict", methods=["GET", "POST"])
-def detect_faces():
-    if request.method == "POST":
-
-<<<<<<< HEAD
-        img = request.files.get("file", "")
-        img = request.form["file"]
-    # test_image=image.load_img(img_path,target_size=(48,48),color_mode='grayscale')
-    # test_image=image.img_to_array(test_image)
-    # test_image=test_image.reshape(1,48,48,1)
-    # classes=['Angry','Disgust','Fear','Happy','Neutral','Sad','Surprise']
-    # result=model.predict(test_image)
-    # y_pred=np.argmax(result[0])
-    # print('The person facial emotion is:',classes[y_pred])
-=======
-    return render_template("./index.html")
-
-@app.route("/post/predict", methods=["GET", "POST"])
-def detect_faces():
-    test_image = request.files["image"]
+get_model()
+def prediction(img_path):
+    new_image = load_image(img_path)
     
-    test_image=image.load_img(test_image,target_size=(48,48),color_mode='grayscale')
-    test_image=image.img_to_array(test_image)
+    result = model.predict(new_image)
+    classes=['Angry','Disgust','Fear','Happy','Neutral','Sad','Surprise']
+    y_pred=np.argmax(result[0])
+    return classes[y_pred]
 
-    test_image = test_image.reshape(1,48,48,1)
-    classes = ['Angry','Disgust','Fear','Happy','Neutral','Sad','Surprise']
-    result = model.predict(test_image)
-    print(result[0])
-    y_pred = np.argmax(result[0])
-    print('The person facial emotion is:',classes[y_pred])
->>>>>>> c2dbe869 (new)
-    return render_template("halaman1.html")
 
+@app.route("/", methods=['GET', 'POST'])                 #initialising
+def home():                                              #function call
+
+    return render_template('index.html')                  #return and calling HTML page (designed template)
+
+
+@app.route("/predict", methods = ['GET','POST'])
+def predict():
+    
+    if request.method == 'POST':
+        
+        file = request.files['file']
+        filename = file.filename
+        file_path = os.path.join("", filename)                       #slashes should be handeled properly
+        file.save(file_path)
+        product = prediction(file_path)
+        
+        if product == "Angry":
+            url = "https://open.spotify.com/search/angry"
+        elif product == "Disgust":
+            url = "https://open.spotify.com/search/disgust"
+        elif product == "Fear":
+            url = "https://open.spotify.com/search/fear"
+        elif product == "Happy":
+            url = "https://open.spotify.com/search/happy"
+        elif product == "Neutral":
+            url = "https://open.spotify.com/search/neutral"
+        elif product == "Sad":
+            url = "https://open.spotify.com/search/sad"
+        elif product == "Surprise":
+            url = "https://open.spotify.com/search/surprise"
+    
+    return render_template('predict.html', product = product, user_image = file_path, url=url)    
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
